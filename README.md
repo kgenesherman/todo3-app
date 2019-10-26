ab073e7a2c4056682c86eb4a67696cd1dbbd3d5a
kgenesherman <kgsherman@sbcglobal.net>
2019-10-26 11:50:01 -0700

    components

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 https://github.com/kgenesherman/todo2-app
+
+initial load
+
+generate components
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { TodoItemComponent } from './todo-item/todo-item.component';
+import { NewTodoItemComponent } from './new-todo-item/new-todo-item.component';
+import { TodoListComponent } from './todo-list/todo-list.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    TodoItemComponent,
+    NewTodoItemComponent,
+    TodoListComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule
